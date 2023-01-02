9395d767e7f8f935f0c5dd60e5b14b828501efa8
Arcadiy <Motor-person@yandex.ru>
2023-01-02 09:23:16 +0400

    Не дораболал калькулятор

---
+++ Practical_7/Calculator/view.py
@@ -0,0 +1,12 @@
+
+
+def input_number():
+    number = int(input('Пожалуйста, введите число:  '))
+    return number
+
+def input_operation():
+    operation = input('Пожалуйста, введите операцию(+, -, *, /, = :  ')
+    return operation
+
+def print_result(smth):
+    print(smth)
